2496f430be42007c946db9d64fe49ecac3314fed
engkan2kit <engkan2kit@gmail.com>
2018-03-24 00:55:54 +0800

    [ADDED] Wifi checking and gateway checking in Splash Screen

--- app/src/main/java/com/tip2panel/smarthome/splash/SplashActivity.java
+++ app/src/main/java/com/tip2panel/smarthome/splash/SplashActivity.java
@@ -16,13 +16,14 @@ import com.tip2panel.smarthome.data.source.local.GatewayLocalDataSource;
 import com.tip2panel.smarthome.gateway.GatewayActivity;
 import com.tip2panel.smarthome.login.LoginActivity;
 import com.crashlytics.android.Crashlytics;
+import com.tip2panel.smarthome.utils.BaseActivity;
 import com.tip2panel.smarthome.utils.DialogUtilities;
 
 import io.fabric.sdk.android.Fabric;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends BaseActivity {
     private  SmartHomeRepository mSmartHomeRepository;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,20 +32,48 @@ public class SplashActivity extends AppCompatActivity {
         mSmartHomeRepository = checkNotNull(SmartHomeRepository.getInstance(
                 GatewayLocalDataSource.getInstance(getApplicationContext())),
                 "smartHomeRepository cannot be null!");
-        mSmartHomeRepository.connectActiveGateway(new GatewayDataSource.GatewayConnectionCallback() {
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mSmartHomeRepository.checkNetworkState(this, new GatewayDataSource.CheckNetworkStateCallback() {
             @Override
-            public void onSuccess(AVA88GatewayInfo ava88GatewayInfo) {
-                Intent intent = new Intent(getApplicationContext(),DashboardActivity.class);
-                startActivity(intent);
-                finish();
+            public void onWifiConnected() {
+                mSmartHomeRepository.connectActiveGateway(new GatewayDataSource.GatewayConnectionCallback() {
+                    @Override
+                    public void onSuccess(AVA88GatewayInfo ava88GatewayInfo) {
+                        Intent intent = new Intent(getApplicationContext(),DashboardActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+
+                    @Override
+                    public void onFailure(int error, AVA88GatewayInfo gatewayInfo) {
+                        DialogUtilities.showGatewayConnectionErrorDialog(SplashActivity.this);
+                    }
+
+                });
             }
 
             @Override
-            public void onFailure(int error, AVA88GatewayInfo gatewayInfo) {
-                DialogUtilities.showGatewayConnectionErrorDialog(getParent());
+            public void onWifiNotConnected() {
+                DialogUtilities.showWifiNotConnectDialog(SplashActivity.this);
             }
 
+            @Override
+            public void onDataConnected() {
+
+            }
+
+            @Override
+            public void onDataNotConnected() {
+
+            }
         });
+
 
 
     }
